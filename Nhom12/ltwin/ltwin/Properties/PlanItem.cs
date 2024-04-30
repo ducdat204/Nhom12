acb8c1c5bf2b9653197568ffef9b6892e73c43f2
Tubuon <168258437+Tubuon@users.noreply.github.com>
2024-04-30 08:47:47 +0700

    Add files via upload

---
+++ Nhom12/ltwin/ltwin/Properties/PlanItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ltwin
+{
+    public class PlanItem
+    {
+        private DateTime date;
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        private string job;
+
+        public string Job
+        {
+            get { return job; }
+            set { job = value; }
+        }
+
+        private Point FromTime;
+        public Point fromTime
+        {
+            get { return FromTime; }
+            set { FromTime = value; }
+        }
+        private Point toTime;
+
+        public Point ToTime
+        {
+            get { return toTime; }
+            set { toTime = value; }
+        }
+
+        private string status;
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public static List<string> ListStatus = new List<string>()
+        { "DONE", "DOING", "COMING", "MISSED" };
+    }
+    public enum EPlanItem
+    {
+        DONE,
+        DOING,
+        COMING,
+        MISSED
+    }
+}
